a6a07c52030ae6721a90ea3fb1e86ac3c8c2d7e0
Paulo Lima <paulolimarb@gmail.com>
2018-08-03 19:14:12 -0300

    change dockerfile and remove unecessary files

--- devops/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PROJECTPATH=$GOPATH/src/github.com/mundipagg/boleto-api
-echo "Changing directory to path $PROJECTPATH"
-cd $PROJECTPATH
-echo "Starting tests"
-go test $(go list ./... | grep -v /vendor/) -v
